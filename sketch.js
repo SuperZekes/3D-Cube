5cd3758d2e46118266ff0f50faf20a812d73401f
SuperZekes <markandsheenaknowles@gmail.com>
2024-04-03 15:02:17 -0400

    Make cube change colors

--- sketch.js
+++ sketch.js
@@ -1,20 +1,21 @@
 function setup() {
   createCanvas(displayWidth, displayHeight, WEBGL);
-
 }
 
 function draw() {
   background(64, 58, 58);
 
-  normalMaterial();
-  push();
-  fill(255)
-  stroke(120)
+  // Calculate color based on frame count
+  let r = map(sin(frameCount * 0.02), -1, 1, 0, 255);
+  let g = map(sin(frameCount * 0.03), -1, 1, 0, 255);
+  let b = map(sin(frameCount * 0.04), -1, 1, 0, 255);
+
+  // Apply color to the cube
+  fill(r, g, b);
+  stroke(120);
   translate(0, -76, 0);
   rotateZ(frameCount * 0.01);
   rotateX(frameCount * 0.01);
   rotateY(frameCount * 0.01);
   box(120);
-  pop();
-
 }
